b6446a0b776ad550e0e992275ae8ff62749cbeeb
suguz06 <68874870+suguz06@users.noreply.github.com>
2020-10-31 10:56:37 -0500

    Initial commit

---
+++ src/test/java/HalloweenTest.java
@@ -0,0 +1,10 @@
+public class HalloweenTest {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World ");
+        System.out.println("Test directory, Halloween java class");
+    }
+}
